fc75c89cc9b4e5088a611863f12d78290fd4e219
Connor Link <connorjlink05@gmail.com>
2024-08-14 16:17:35 -0700

    Revert "Fix common favicon styling"
    This reverts commit 70d466e52c0974bf533de777c94750abfe3ee4ce.

--- common.js
+++
@@ -1,7 +0,0 @@
-const head = document.querySelector('head');
-
-// inject common styles
-head.insertAdjacentHTML("beforeend", `
-    <link rel="stylesheet" href="common.css">
-    <meta property="og:image" content="https://connorjlink.com/res/logo.jpg"/>
-`);
