1af40d3931fa83dc47543396922680a93d212ba4
Connor Link <connor@connorjlink.com>
2025-07-26 19:19:47 -0500

    Update resume

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { exec } from 'child_process';
 import fs from 'fs';
 
 function getHtmlInputs() {
@@ -13,20 +14,41 @@ function getHtmlInputs() {
 }
 
 function InjectorPlugin() {
-    return {
-        name: 'injector-plugin',
-        handleHotUpdate({ file, server }) {
-            if (
-                file.endsWith('.html') ||
-                file.startsWith(resolve(__dirname, 'partials'))
-            ) {
-                execSync('node injector.cjs');
-                server.ws.send({ type: 'full-reload' });
-            }
-        }
-    };
-}
+   return {
+      name: 'injector-plugin',
+      async handleHotUpdate({ file, server }) {
+         if (file.includes('temp')) {
+            return;
+         }
+         if (
+            file.endsWith('.html') ||
+            file.startsWith(resolve(__dirname, 'partials'))
+         ) {
+            var hadError = false;
 
+            exec('node injector.cjs', (error, stdout, stderr) => {
+               if (error) {
+                  console.error(`Injector error: ${error.message}`);
+                  hadError = true;
+                  return;
+               }
+               console.log(stdout);
+            });
+
+            exec('node renamer.cjs', (error, stdout, stderr) => {
+               if (error) {
+                  console.error(`Renamer error: ${error.message}`);
+                  hadError = true;
+                  return;
+               }
+               console.log(stdout);
+            });
+
+            server.ws.send({ type: 'full-reload' });
+         }
+      }
+   };
+}
 
 export default defineConfig({
     plugins: [InjectorPlugin()],
